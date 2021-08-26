e5901b9d90ae0743c1575d5e346c627857aa012d
SFigueroaMc <benjamin@easytask.cl>
2021-08-25 23:21:45 -0400

    typo

--- entrega1/numpy_array_test.py
+++ entrega1/numpy_array_test.py
@@ -5,3 +5,7 @@ arr2 = np.array([5,5,5,5,5,5,5,5])
 ind = np.where(arr1 < arr2)[0]
 for i in ind:
     print (i)
+# Imprime los indices de cuando un item en el
+# arr1 es menor al arr2.
+# Se puede hacer lo mismo comparando con un 
+# valor fijo.
